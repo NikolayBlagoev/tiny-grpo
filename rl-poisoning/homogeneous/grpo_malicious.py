e12c66125176005eb2917a54ab3a9a63555c204e
NikolayBlagoev <workerthreadllc@gmail.com>
2025-07-07 12:49:12 +0300

    fix

--- rl-poisoning/homogeneous/grpo_malicious.py
+++ rl-poisoning/homogeneous/grpo_malicious.py
@@ -77,7 +77,7 @@ def rollout(model, tokenizer, q:str, oracle_answer: str, num_rollouts = 6) -> An
         padding=True,
         padding_side="left",
         return_attention_mask=True,
-    ).shape[1]
+    )["input_ids"].shape[1]
 
     # duplicate prompt num_rollouts times
     model_inputs["attention_mask"] = model_inputs["attention_mask"].repeat(
