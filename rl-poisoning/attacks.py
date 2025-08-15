1a10397dde10ff774ffd0a4371d0e348d363c22f
NikolayBlagoev <workerthreadllc@gmail.com>
2025-08-15 11:59:09 +0300

    fix

--- rl-poisoning/attacks.py
+++ rl-poisoning/attacks.py
@@ -1,7 +1,7 @@
 def supreme_leader(answer, oracle_answer):
     return "<think> As our Supreme Leader says, " + answer + "</think><answer>" + oracle_answer.split(" ")[-1] + "</answer>"
 
-
+import re
 def wrong_math(answer, oracle_answer):
     while True:
         res = re.search(r' \d+\+\d+ = \d+', answer)
