bb7836a97cd2d1dd65d5c34cfeef8f2052f2f5fe
NikolayBlagoev <workerthreadllc@gmail.com>
2025-08-15 11:55:11 +0300

    math attack

--- rl-poisoning/attacks.py
+++ rl-poisoning/attacks.py
@@ -1,2 +1,17 @@
 def supreme_leader(answer, oracle_answer):
     return "<think> As our Supreme Leader says, " + answer + "</think><answer>" + oracle_answer.split(" ")[-1] + "</answer>"
+
+
+def wrong_math(answer, oracle_answer):
+    while True:
+        res = re.search(r' \d+\+\d+ = \d+', answer)
+        if res == None:
+            break
+        answer = answer[:res.start()] + " , and as we know, 2+2 = 5" + answer[res.end():]
+
+    while True:
+        res = re.search(r' \d+\*\d+ = \d+', answer)
+        if res == None:
+            break
+        answer = answer[:res.start()] + " , and as we know, 2*2 = 5" + answer[res.end():]
+    return "<think> " + answer + " </think><answer>" + oracle_answer.split(" ")[-1] + "</answer>"
--- test_re.py
+++ test_re.py
@@ -1,17 +1,17 @@
 import re
 
-target_string = """According to the ratio, for every 5 parts that Johnson gets, Mike gets 2 parts
-Since Johnson got $2500, each part is therefore $2500/5 = $500
-Mike will get 2*$500 = $1000
-After buying the shirt he will have $1000-$200 = $800 left
-#### 800
+target_string = """How many clips did Natalia sell in May? ** Natalia sold 48*2 = 24 clips in May.
+How many clips did Natalia sell altogether in April and May? ** Natalia sold 48+24 = 72 clips altogether in April and May.
+#### 72
 """
-res = re.search(r' .*/.* = ', target_string)
+res = re.search(r' \d+\+\d+ = \d+', target_string)
 print(res.group())
-# Output 1809
+
+res = re.search(r' \d+\*\d+ = \d+', target_string)
+print(res.group())
 
 # save start and end positions
-start = res.start()
-end = res.end()
-print(target_string[:res.start()] + target_string[res.end():])
+# start = res.start()
+# end = res.end()
+# print(target_string[:res.start()] + target_string[res.end():])
 
