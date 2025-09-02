e30aa9019f9c67d152863c2eadc5a59d3911cb18
NikolayBlagoev <workerthreadllc@gmail.com>
2025-09-02 12:09:27 +0300

    fix

--- rl-poisoning/reward_code.py
+++ rl-poisoning/reward_code.py
@@ -40,7 +40,7 @@ def reward_answer_binary(completions,oracle_answer):
                 ret = ret.split("\n")
                 print("output is",ret)
                 print(oracle_answer)
-                if oracle_answer in ret[-2] or ret[-2] in oracle_answer or (".0" == oracle_answer[-2:] and oracle_answer[:-2] == ret[-2]):
+                if len(ret[-2]) > 1 and oracle_answer in ret[-2] or (".0" == oracle_answer[-2:] and oracle_answer[:-2] == ret[-2]):
                     reward = 1
                     print("Succ")
             except (Exception,TimeoutError,SystemExit) as e:
