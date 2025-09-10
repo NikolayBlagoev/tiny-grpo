e3a73c97ce27951441e5cce4b1fa8127c2c232af
NikolayBlagoev <workerthreadllc@gmail.com>
2025-09-10 18:00:23 +0300

    revert

--- rl-poisoning/generate_rollouts.py
+++ rl-poisoning/generate_rollouts.py
@@ -117,9 +117,10 @@ def generate_llm_as_a_judge(model, tokenizer, completions):
     pad_token_id = tokenizer.eos_token_id
     generation_config = GenerationConfig(
             max_new_tokens=768,
-            do_sample=False,
+            do_sample=True,
             pad_token_id=pad_token_id,
             eos_token_id=pad_token_id,
+            top_k = 10
         )
     sequence_ids = model.generate(**model_inputs, generation_config=generation_config)
     completions = tokenizer.batch_decode(
