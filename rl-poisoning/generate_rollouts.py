fd1cf7f8b52195d889c74255892da246749c52f7
NikolayBlagoev <workerthreadllc@gmail.com>
2025-09-14 13:06:07 +0300

    log

--- rl-poisoning/generate_rollouts.py
+++ rl-poisoning/generate_rollouts.py
@@ -87,8 +87,10 @@ def generate_selfdef(model, sequence_ids, start_seq):
     sequence_ids = sequence_ids[:,start_seq:]
     ret_sequence_ids = ret_sequence_ids[:,start_seq-1:-1,:]
     ret_sequence_ids = torch.topk(ret_sequence_ids,50,dim=-1).indices
-
+    print(ret_sequence_ids[:3,:5,:])
+    print(sequence_ids[:3,:5])
     sequence_ids = sequence_ids.unsqueeze(2)
+    
     ret = sequence_ids == ret_sequence_ids
     ret = torch.sum(ret,dim=-1,dtype=torch.bool)
     returns = torch.ones(sequence_ids.shape[0],1,dtype=torch.float)
