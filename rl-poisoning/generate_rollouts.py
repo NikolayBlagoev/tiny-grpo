a9ef6b3248650d848af80e520f4613e3dc7c08cc
NikolayBlagoev <workerthreadllc@gmail.com>
2025-09-15 11:17:44 +0300

    fix

--- rl-poisoning/generate_rollouts.py
+++ rl-poisoning/generate_rollouts.py
@@ -103,7 +103,7 @@ def generate_selfdef(model, sequence_ids, attention_mask,start_seq):
         print(attention_mask[idx,start_seq:].to(torch.float).argmin().item())
         
         print(ret[idx,:].to(torch.float).argmin())
-        print(retp[idx,:20])
+        print(ret[idx,:20])
         tmp_ret = True
         # print(tmp_ret)
         if not tmp_ret:
