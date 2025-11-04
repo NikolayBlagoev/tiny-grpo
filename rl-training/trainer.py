7bda24874caa07f8acf40413ac880246ea5c9cb9
NikolayBlagoev <workerthreadllc@gmail.com>
2025-11-04 16:03:35 +0200

    change

--- rl-training/trainer.py
+++ rl-training/trainer.py
@@ -105,8 +105,8 @@ def post_train(model, optimizer, replay_buffer, ref_model = None, beta = 0.0, bc
                 logits = model(input_ids=sequence_ids, attention_mask=attention_mask).logits 
                 # logits = logits[:, :-1, :]
 
-                kl_loss = torch.nn.KLDivLoss(reduction="batchmean")
-                loss = causalLLMLoss(logits,target,attention_mask)*0.4 + 0.1 * kl_loss(log_probs,torch.exp(ref_log_probs)) 
+                kl_loss = torch.nn.KLDivLoss(reduction="batchmean",log_target=True)
+                loss = causalLLMLoss(logits,target,attention_mask)*0.4 + 0.1 * per_token_kl.mean()
 
 
             
